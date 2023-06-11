842ccf508548091f6125a44355c8341bd4d43187
aqira <aqira.kelana@gmail.com>
2023-06-11 18:41:45 +0700

    init commit

---
+++ bikestores/src/main/java/com/tutorial/bikestores/BikestoresApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.bikestores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikestoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikestoresApplication.class, args);
+	}
+
+}
